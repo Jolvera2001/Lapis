dc0981679e5ed1f22eecc012e201d3f696b548b4
Jolvera2001 <jolvera6@stedwards.edu>
2024-11-01 23:38:15 -0500

    now it works

--- internal/utils/app.go
+++ internal/utils/app.go
@@ -1,10 +1,12 @@
 package utils
 
 import (
+	"image/color"
 	"lapis-project/internal/page"
 	"lapis-project/internal/state"
 
 	"gioui.org/layout"
+	"gioui.org/op/paint"
 	"gioui.org/widget/material"
 )
 
@@ -28,10 +30,19 @@ func NewApp() *App {
 }
 
 func (a *App) Layout(gtx layout.Context) layout.Dimensions {
+	paint.Fill(gtx.Ops, a.getBackgroundColor())
+
 	switch a.state.CurrentPage {
 	case state.MainPageID:
 		return a.main.Layout(gtx)
 	default: 
 		return a.main.Layout(gtx)
 	}
+}
+
+func (a *App) getBackgroundColor() color.NRGBA {
+    if a.state.DarkMode {
+        return color.NRGBA{R: 0x12, G: 0x12, B: 0x12, A: 0xFF}
+    }
+    return color.NRGBA{R: 0xF8, G: 0xF9, B: 0xFA, A: 0xFF}
 }
