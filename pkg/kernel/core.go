52e85592f8f3a364fd77831c00d6091f2bbf69f7
Jolvera2001 <jolvera6@stedwards.edu>
2024-11-02 22:57:51 -0500

    added kernel Register func

--- pkg/kernel/core.go
+++ pkg/kernel/core.go
@@ -1,6 +1,8 @@
 package kernel
 
 import (
+	"errors"
+	"fmt"
 	i "lapis-project/pkg/api/interfaces"
 	"sync"
 )
@@ -9,4 +11,27 @@ type Kernel struct {
 	plugins map[string]i.Plugin
 	mu      sync.Mutex
 	started bool
+}
+
+func NewKernel() *Kernel {
+	return &Kernel{
+		plugins: make(map[string]i.Plugin),
+	}
+}
+
+func (k *Kernel) Register(p i.Plugin) error {
+	k.mu.Lock()
+	defer k.mu.Unlock()
+
+	if k.started {
+		return errors.New("cannot register plugin: kernel already started")
+	}
+
+	id := p.ID()
+	if _, exists := k.plugins[id]; exists {
+		return fmt.Errorf("plugin %s already registered", id)
+	}
+
+	k.plugins[id] = p
+	return nil
 }
