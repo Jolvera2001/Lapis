4c93aad8b6214a555a45f1dc5c6704ebb9994312
Jolvera2001 <jolvera6@stedwards.edu>
2024-11-06 19:21:56 -0600

    added a layout manager concept

---
+++ pkg/kernel/layout.go
@@ -0,0 +1,35 @@
+package kernel
+
+import (
+	"fmt"
+	"sync"
+)
+
+type LayoutManager struct {
+	zones             map[string][]UIPlug
+	validDestinations map[string]bool
+	mu                sync.Mutex
+}
+
+func NewLayoutManager() *LayoutManager {
+	return &LayoutManager{
+		zones: make(map[string][]UIPlug),
+		validDestinations: map[string]bool{
+			"left-sidebar": true,
+			"main-area": true,
+			"header": true,
+		},
+	}
+}
+
+func (l *LayoutManager) AddWidget(plug UIPlug) error {
+	l.mu.Lock()
+	defer l.mu.Unlock()
+
+	if !l.validDestinations[plug.Destination] {
+		return fmt.Errorf("invalid destination: %s", plug.Destination)
+	}
+
+	l.zones[plug.Destination] = append(l.zones[plug.Destination], plug)
+	return nil
+}
