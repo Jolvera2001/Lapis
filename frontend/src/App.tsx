696de6ddf7b9efd121882850433340378cb564eb
Jolvera2001 <jolvera6@stedwards.edu>
2024-11-20 10:58:54 -0600

    basic editor added

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,16 @@
+import { SetStateAction, useCallback, useState } from "react";
+import ReactCodeMirror, { ViewUpdate } from "@uiw/react-codemirror";
+import { markdown } from "@codemirror/lang-markdown";
 
 
 function App() {
+    const [value, setValue] = useState<string | undefined>("# Hello world!")
+    const onChange = useCallback((val: SetStateAction<string | undefined>, ViewUpdate: any) => {
+        console.log('val:', val );
+        setValue(val);
+    }, [])
 
-    return (
-        <>
-        </>
-    )
+    return <ReactCodeMirror value={value}  height="300px" extensions={[markdown()]} onChange={onChange} />;
 }
 
 export default App
