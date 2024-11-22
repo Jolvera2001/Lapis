f52a113faabf056446c5ce37d2a7b468888940a8
Jolvera2001 <jolvera6@stedwards.edu>
2024-11-21 22:24:48 -0600

    weird thing with icons. but minimal ui working ig

--- frontend/src/features/editor/page/editorPage.tsx
+++ frontend/src/features/editor/page/editorPage.tsx
@@ -1,37 +1,55 @@
-import { Container, Stack } from "@mui/joy"
+import { Button, ButtonGroup, Container, IconButton, Stack, Typography } from "@mui/joy"
 import MarkdownEditor from "../components/editor"
 import { SimpleTreeView, TreeItem } from "@mui/x-tree-view"
+import { useState } from "react"
+import { Add } from "@mui/icons-material"
 
 function EditorPage() {
+    const [currentFolder, setCurrentFolder] = useState("test")
+
     return(
         <Stack 
             direction="row"
             spacing={0}
             sx={{
-                position: 'fixed', // Fix the position
+                position: 'fixed',
                 height: '100vh',
                 width: '100vw',
                 overflow: 'hidden',
               }}
         >
             <Stack
+                spacing={1}
                 sx={{
-                    width: '240px', // Fixed width for sidebar
+                    width: '240px',
                     height: '100%',
                     borderRight: '1px solid',
                     borderColor: 'divider',
-                    overflow: 'auto', // Allow scrolling in tree view
+                    overflow: 'auto',
+                    padding: '5px'
                 }}
             >
+                <Stack
+                    direction="row"
+                    spacing={2}
+                >
+                    <Typography
+                        level="h3"
+                    >
+                        {currentFolder}
+                    </Typography> 
+                    <Button startDecorator={<Add />}>New Folder</Button>
+                </Stack>
                 <SimpleTreeView>
                     <TreeItem label="someLabel" itemId="1" />
                 </SimpleTreeView>
             </Stack>
             <Stack
+                spacing={0}
                 sx={{
-                    flex: 1, // Take remaining space
+                    flex: 1,
                     height: '100%',
-                    overflow: 'hidden', // Important for editor
+                    overflow: 'hidden',
                 }}
             >
                 <MarkdownEditor
