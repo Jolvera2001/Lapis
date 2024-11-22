ba96c0c6fdab89399d5bd35c518011768b8c8254
Jolvera2001 <jolvera6@stedwards.edu>
2024-11-21 20:10:34 -0600

    more removing stuff

--- pkg/kernel/api.go
+++
@@ -1,8 +0,0 @@
-package kernel
-
-type API interface {
-	Subscribe(eventName string, handler EventHandler)
-	Emit(event Event) error
-	AddUIPlug(pluginId string, uiPlug UIPlug) error
-	GetPlugin(id string) Plugin
-}
--- pkg/kernel/plugin.go
+++
@@ -1,9 +0,0 @@
-package kernel
-
-type Plugin interface {
-	ID() string
-	Dependencies() []string
-	Initialize(api API) error
-	Start() error
-	Stop() error
-}
